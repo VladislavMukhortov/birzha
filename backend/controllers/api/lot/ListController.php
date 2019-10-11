1c0797aec239d7c87ffa340818e71a74e0ed1e53
Ceyl0n <ceyl0n@yandex.ru>
2019-10-11 13:28:30 +0300

    Страница с моими объявлениями

--- backend/controllers/api/lot/ListController.php
+++ backend/controllers/api/lot/ListController.php
@@ -8,6 +8,7 @@ use yii\rest\Controller;
 use yii\helpers\ArrayHelper;
 use yii\filters\Cors;
 use yii\filters\VerbFilter;
+use yii\filters\auth\HttpHeaderAuth;
 use yii\web\Response;
 
 use yii\db\Query;
@@ -28,7 +29,7 @@ class ListController extends Controller
      */
     public function behaviors() : array
     {
-        return ArrayHelper::merge(parent::behaviors(), [
+        $behaviors = [
             'corsFilter' => [
                 'class' => Cors::className(),
                 'cors' => [
@@ -45,8 +46,23 @@ class ListController extends Controller
                 'actions' => [
                     'index' => ['GET', 'POST', 'OPTIONS']
                 ]
-            ]
-        ]);
+            ],
+        ];
+
+        /**
+         * если передается токен "x-api-key?, тогда проходим авторизацию по токену,
+         * что бы получить данные пользователя который обращается к экшену
+         * @var [type]
+         */
+        $user_api_key = Yii::$app->request->getHeaders()['x-api-key'] ?? null;
+        if ($user_api_key) {
+            $behaviors['authenticator'] = [
+                'class' => HttpHeaderAuth::className(),
+                'except' => ['options']
+            ];
+        }
+
+        return ArrayHelper::merge(parent::behaviors(), $behaviors);
     }
 
 
@@ -211,9 +227,99 @@ class ListController extends Controller
      *
      * @return
      */
-    public function actionMy() : Response
+    public function actionMyOrders() : Response
     {
-        return $this->asJson();
+        $query = (new Query())
+            ->select([
+                "id",
+                "crop_id",
+                "deal",
+                "price",
+                "currency",
+                "quantity",
+                "period",
+                "basis",
+                "fob_port",
+                "fob_terminal",
+                "cif_country",
+                "cif_port",
+                "moisture",
+                "foreign_matter",
+                "grain_admixture",
+                "gluten",
+                "protein",
+                "natural_weight",
+                "falling_number",
+                "vitreousness",
+                "ragweed",
+                "bug",
+                "oil_content",
+                "oil_admixture",
+                "broken",
+                "damaged",
+                "dirty",
+                "ash",
+                "erucidic_acid",
+                "peroxide_value",
+                "acid_value",
+                "link",
+                "status",
+                "created_at",
+            ])
+            ->from(Lot::tableName())
+            ->where([
+                "company_id" => Yii::$app->user->identity->company_id,
+                "status" => [
+                    Lot::STATUS_ARCHIVE,
+                    Lot::STATUS_WAITING,
+                    Lot::STATUS_ACTIVE,
+                    Lot::STATUS_COMMUNICATION,
+                    Lot::STATUS_COMPLETE
+                ],
+            ])
+            ->orderBy([
+                "created_at" => SORT_DESC,
+                "id" => SORT_DESC,
+            ]);
+
+        $data_provider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => Lot::LOT_ON_PAGE
+            ]
+        ]);
+
+        $lots = $data_provider->getModels();
+        $data = [];
+
+        $crops = ArrayHelper::map(Crops::find()->allArray(), 'id', 'name');
+
+        for ($i = 0, $count = count($lots); $i < $count; $i++) {
+            $data[$i] = [
+                'title' => Yii::t('app', 'crops.' . $crops[$lots[$i]['crop_id']]),
+                'deal' => strval($lots[$i]['deal']),
+                'basis' => strval($lots[$i]['basis']),
+                'price' => Yii::$app->formatter->asCurrency($lots[$i]['price'], $lots[$i]['currency']),
+                'quantity' => strval($lots[$i]['quantity']),
+                'period' => strval($lots[$i]['period']),
+                'link' => strval($lots[$i]['link']),
+                'created_at' => strval($lots[$i]['created_at']),
+                'basis_location' => Lot::getBasisLocationArray($lots[$i]),
+                'quality' => Lot::getStrQualityArray($lots[$i]),
+            ];
+
+            $st = (int) $lots[$i]['status'];
+            if ($st === Lot::STATUS_ARCHIVE) {$data[$i]['status'] = 'STATUS_ARCHIVE';}
+            if ($st === Lot::STATUS_WAITING) {$data[$i]['status'] = 'STATUS_WAITING';}
+            if ($st === Lot::STATUS_ACTIVE) {$data[$i]['status'] = 'STATUS_ACTIVE';}
+            if ($st === Lot::STATUS_COMMUNICATION) {$data[$i]['status'] = 'STATUS_COMMUNICATION';}
+            if ($st === Lot::STATUS_COMPLETE) {$data[$i]['status'] = 'STATUS_COMPLETE';}
+        }
+
+        return $this->asJson([
+            'data' => $data,
+            'pagination_page_count' => $data_provider->getPagination()->getPageCount(), // кол-во страниц с результатами
+        ]);
     }
 
 }
--- frontend/pages/orders/index.vue
+++ frontend/pages/orders/index.vue
@@ -1,14 +1,42 @@
+// Страница с объявлениями которые подал пользователь
+
 <template>
 
     <main>
         <section class="section_block">
             <b-container>
-                <b-row>
+                <b-row class="justify-content-center">
                     <b-col cols="12" md="8">
 
-                        <h1 class="section_title">My orders </h1>
+                        <h1 class="section_title">My orders</h1>
 
-                        <pre>{{orders}}</pre>
+                        <b-pagination-nav
+                            v-model="page_number"
+                            v-bind:link-gen="linkGen"
+                            v-bind:number-of-pages="pagination_page_count"
+                            no-page-detect
+                            use-router></b-pagination-nav>
+
+                        <b-list-group>
+                            <b-list-group-item v-for="item in lots">
+                                <div class="order-item">
+                                    <h4 class="order-item-title">
+                                        <span>{{ item.title }}</span>
+                                        <span class="order-item-price">{{ item.price }}</span>
+                                    </h4>
+                                    <div class="order-item-desc">
+                                        <span>{{ item.deal }}</span>
+                                        <span class="order-item-price">{{ item.quantity }} тонн</span>
+                                    </div>
+                                    <div>{{ item.quality }}</div>
+                                    <div>{{ item.period }}</div>
+                                    <div><b>{{ item.basis }}</b> | {{ item.basis_location }}</div>
+                                    <div><b>Создано:</b> {{ item.created_at }}</div>
+                                    <div class="text-danger">Статус: {{ item.status }}</div>
+                                    <b-link v-bind:to="'/orders/edit/'+item.link">Редактировать</b-link>
+                                </div>
+                            </b-list-group-item>
+                        </b-list-group>
 
                     </b-col>
                 </b-row>
@@ -22,33 +50,104 @@
 
 <script>
 export default {
-    auth: false,
-
     head() {
         return {
             title: 'My orders | site.com',
-            meta: [{ hid: 'description', name: 'description', content: '' }]
+            meta: [{ hid: 'description', name: 'description', content: '' }],
         }
     },
 
     data() {
         return {
-            orders: {}
+            lots: {},                                 // сделки (офферы)
+            page_number: this.$route.query.page || 1,   // текущая страница
+            pagination_page_count: 0,                   // кол-во страниц с результатами
         }
     },
 
-    async asyncData ({ $axios }) {
-        const orders = await $axios.$get('/api/messages/dialogs/index').then((res) => {
+    /**
+     * Получаем список объявлений
+     * @param  {[type]} options.$axios
+     * @param  {[type]} options.route
+     * @return Object                  список объявлений, кол-во страниц с объявлениями
+     */
+    async asyncData({ $axios, route }) {
+        let _param = {params: {
+            page: route.query.page || 1
+        }};
+
+        const res = await $axios.$get('/api/lot/list/my-orders', _param).then((res) => {
             return res;
         });
 
-        return { orders: orders.data };
-    }
+        return {
+            lots: res.data,
+            pagination_page_count: res.pagination_page_count,
+        };
+    },
+
+    watch: {
+        /**
+         * При изменении текущей страницы загружаем список объявлений для нее
+         */
+        page_number: function() {
+            this.updateLotData();
+        },
+    },
+
+    methods: {
+        /**
+         * Переключение пагинации через URL
+         * @param  integer page_num номер страницы на которую переходим
+         * @return string  url params
+         */
+        linkGen(page_num) {
+            return `?page=${page_num}`;
+        },
+
+        /**
+         * Загрузка списка объявлений
+         */
+        async updateLotData() {
+            let _param = {params: {
+                page: this.page_number
+            }};
+
+            /**
+             * @param  Object res список объявлений
+             */
+            let res = await this.$axios.$get('/api/lot/list/my-orders', _param).then((res) => {
+                return res;
+            })
+
+            this.lots = res.data;
+            this.pagination_page_count = res.pagination_page_count;
+        },
+    },
+
 };
 </script>
 
 
 
 <style lang='scss'>
+.order-item {
+}
 
+.order-item-title {
+    position: relative;
+    padding: 0 150px 0 0;
+    text-transform: uppercase;
+}
+
+.order-item-price {
+    position: absolute;
+    top: 0;
+    right: 0;
+}
+
+.order-item-desc {
+    position: relative;
+    padding: 0 150px 0 0;
+}
 </style>
